7d1999a31fb363f245d287f753c0084bdfccb03d
vibhav mishra <mishravibhav1910@gmail.com>
2021-05-21 00:54:42 +0530

    Job card created

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import "./Home.css";
 import SearchIcon from "@material-ui/icons/Search";
 import RoomIcon from "@material-ui/icons/Room";
 
 function Home(props) {
-  console.log(props.Mode);
+  //   console.log(props.Mode);
+
+  const [Data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await Axios.get(
+        `https://jobs.github.com/positions.json?description=python&location=USA`
+      );
+      setData(response.data);
+      // console.log(response.data)
+    };
+
+    fetchData();
+  }, []);
+  console.log(Data);
   return (
     <div className="Home">
       <form>
@@ -21,7 +37,7 @@ function Home(props) {
           <div className="Searchbar__items">
             <RoomIcon />
             <input
-            className="search__input"
+              className="search__input"
               type="text"
               placeholder="Search by title, companies, expertise"
             />
@@ -36,15 +52,31 @@ function Home(props) {
             />
             <label for="full-time">Full Time Only</label>
 
-            <input type="submit" className="search__button btn btn-primary" value="Search" />
+            <input
+              type="submit"
+              className="search__button btn btn-primary"
+              value="Search"
+            />
           </div>
         </div>
       </form>
 
       <div className="Jobposts">
-            <div>
-                
+        {
+            Data.map((item)=>(<div className="Job_Card card shadow">
+            <div ><img className="Jobposts__img" src={item.company_logo}/></div>
+            <div className="Jobposts__desc">
+              <div style={{display:"flex",flexDirection:"row"}}>
+                <p style={{ color: "grey", fontSize: "20px" }}></p>
+                <p style={{ color: "grey", fontSize: "10px",margin:"7px" }}>⚫</p>
+                <p style={{ color: "grey", fontSize: "20px" }}>{item.type}</p>
+              </div>
+              <h4>{item.title}</h4>
+              <p style={{ color: "grey", fontSize: "20px", marginTop:"15px" }}>{item.company} </p>
+              <p style={{ color: "blue", fontSize: "20px" }}>{item.location}</p>
             </div>
+          </div>))
+        }
       </div>
     </div>
   );
